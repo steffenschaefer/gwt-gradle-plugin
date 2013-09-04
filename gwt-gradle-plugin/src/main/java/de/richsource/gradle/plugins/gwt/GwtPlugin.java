17b3eba92c6be84f8df500d0b41ff281ce4a33ae
Steffen Schäfer <steffen.schafer@googlemail.com>
2013-09-04 17:02:46 +0200

    Extends war creation with gwt compiler output

--- gwt-gradle-plugin/src/main/java/de/richsource/gradle/plugins/gwt/GwtPlugin.java
+++ gwt-gradle-plugin/src/main/java/de/richsource/gradle/plugins/gwt/GwtPlugin.java
@@ -2,6 +2,8 @@ package de.richsource.gradle.plugins.gwt;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.plugins.WarPlugin;
+import org.gradle.api.tasks.bundling.War;
 
 public class GwtPlugin implements Plugin<Project> {
 	public static final String EXTENSION_NAME = "gwt";
@@ -9,7 +11,14 @@ public class GwtPlugin implements Plugin<Project> {
 	@Override
 	public void apply(Project project) {
 		project.getPlugins().apply(GwtBasePlugin.class);
-		GwtPluginExtension gwtPluginExtension = project.getExtensions().getByType(GwtPluginExtension.class);
+		project.getPlugins().apply(WarPlugin.class);
+		
+		GwtPluginExtension extension = project.getExtensions().getByType(GwtPluginExtension.class);
+		GwtCompileTask compileTask = (GwtCompileTask) project.getTasks().getByName(GwtCompileTask.NAME);
+		War warTask = (War) project.getTasks().getByName(WarPlugin.WAR_TASK_NAME);
+
+		warTask.dependsOn(compileTask);
+		warTask.from(compileTask.getWar());
 	}
 
 }
