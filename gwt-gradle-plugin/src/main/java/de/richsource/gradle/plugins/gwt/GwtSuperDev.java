4901c01ba1a2c4dd496c627edc5ce5f0de388e1b
Steffen Schäfer <steffen.schafer@googlemail.com>
2013-09-10 18:08:38 +0200

    Addition of GwtSuperDev configuration properties

--- gwt-gradle-plugin/src/main/java/de/richsource/gradle/plugins/gwt/GwtSuperDev.java
+++ gwt-gradle-plugin/src/main/java/de/richsource/gradle/plugins/gwt/GwtSuperDev.java
@@ -2,13 +2,23 @@ package de.richsource.gradle.plugins.gwt;
 
 import java.io.File;
 
+//CodeServer [-bindAddress address] [-port port] [-workDir dir] [-src
+// dir] [module]
+//
+// where
+// -bindAddress The ip address of the code server. Defaults to 127.0.0.1.
+// -port The port where the code server will run.
+// -workDir The root of the directory tree where the code server will write compiler output. If not supplied, a temporary directory will be used.
+// -src A directory containing GWT source to be prepended to the classpath for compiling.
+// and
+// module The GWT modules that the code server should compile. (Example: com.example.MyApp)
 public class GwtSuperDev extends AbstractGwtActionTask {
 	
 	private File workDir;
 
-	// private Boolean noserver;
-	// private Integer port;
-	// private String whitelist;
+	private String bindAddress;
+	private Integer port;
+	private Boolean noPrecompile;
 
 	@Override
 	protected String getClassName() {
@@ -24,36 +34,9 @@ public class GwtSuperDev extends AbstractGwtActionTask {
 			}
 		}
 		dirArgIfSet("-workDir", getWorkDir());
-
-		// CodeServer [-bindAddress address] [-port port] [-workDir dir] [-src
-		// dir] [module]
-		//
-		// where
-		// -bindAddress The ip address of the code server. Defaults to
-		// 127.0.0.1.
-		// -port The port where the code server will run.
-		// -workDir The root of the directory tree where the code server will
-		// write compiler output. If not supplied, a temporary directory will be
-		// used.
-		// -src A directory containing GWT source to be prepended to the
-		// classpath for compiling.
-		// and
-		// module The GWT modules that the code server should compile. (Example:
-		// com.example.MyApp)
-
-		// private boolean noPrecompile = false;
-		// private boolean isCompileTest = false;
-		// private File workDir;
-		// private List<String> moduleNames = new ArrayList<String>();
-		// private boolean allowMissingSourceDir = false;
-		// private final List<File> sourcePath = new ArrayList<File>();
-		// private String bindAddress = "127.0.0.1";
-		// private String preferredHost = "localhost";
-		// private int port = 9876;
-		// private RecompileListener recompileListener = RecompileListener.NONE;
-		// // Use the same default as the GWT compiler.
-		// private SourceLevel sourceLevel = SourceLevel.DEFAULT_SOURCE_LEVEL;
-
+		argIfSet("-bindAddress", getBindAddress());
+		argIfSet("-port", getPort());
+		argIfEnabled(getNoPrecompile(), "-noprecompile");
 	}
 	
 	protected boolean prependSrcToClasspath() {
@@ -66,5 +49,29 @@ public class GwtSuperDev extends AbstractGwtActionTask {
 
 	public void setWorkDir(File workDir) {
 		this.workDir = workDir;
+	}
+
+	public String getBindAddress() {
+		return bindAddress;
+	}
+
+	public void setBindAddress(String bindAddress) {
+		this.bindAddress = bindAddress;
+	}
+
+	public Integer getPort() {
+		return port;
+	}
+
+	public void setPort(Integer port) {
+		this.port = port;
+	}
+
+	public Boolean getNoPrecompile() {
+		return noPrecompile;
+	}
+
+	public void setNoPrecompile(Boolean noPrecompile) {
+		this.noPrecompile = noPrecompile;
 	}
 }
