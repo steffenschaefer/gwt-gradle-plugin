086cde5b6d28dc5ded83890226f4d8c6f63ba0db
Steffen Schäfer <steffen.schafer@googlemail.com>
2013-10-20 15:37:12 +0200

    Typo

--- gwt-gradle-plugin/src/main/java/de/richsource/gradle/plugins/gwt/Style.java
+++ gwt-gradle-plugin/src/main/java/de/richsource/gradle/plugins/gwt/Style.java
@@ -16,7 +16,7 @@
 package de.richsource.gradle.plugins.gwt;
 
 /**
- * Output styles suported by the GWT compiler.
+ * Output styles supported by the GWT compiler.
  */
 public enum Style {
 	/** Obfuscated or minimized style. This should be used for production. */
