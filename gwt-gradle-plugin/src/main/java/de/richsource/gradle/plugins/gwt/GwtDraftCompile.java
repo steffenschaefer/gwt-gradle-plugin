ad8c740b3765b4329f2dc58a4cfc1017814e03b9
Steffen Schäfer <steffen.schafer@googlemail.com>
2014-09-08 17:41:51 +0200

    License Header Update

--- gwt-gradle-plugin/src/main/java/de/richsource/gradle/plugins/gwt/GwtDraftCompile.java
+++ gwt-gradle-plugin/src/main/java/de/richsource/gradle/plugins/gwt/GwtDraftCompile.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2013 Steffen Schaefer
+ * Copyright (C) 2013-2014 Steffen Schaefer
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
